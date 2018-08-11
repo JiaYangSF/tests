72d417a3e30fa8fc0ecc4a6e29371d3303f1551c
JiaYang <sandyjaja2008@gmail.com>
2018-08-10 20:36:40 -0700

    jia

---
+++ jia/src/main/java/test/jia/App.java
@@ -0,0 +1,13 @@
+package test.jia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Jia" );
+    }
+}
